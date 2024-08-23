cd1d4554ead45abbfb641b6561d3c6ad05dc9751
HAMADANTER999 <antermohammed390@gmail.com>
2024-08-23 14:05:35 +0300

    add interrupt files

---
+++ MCAL_Layer/interrupt/mcal_internal_interrupts.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   mcal_internal_interrupts.h
+ * Author: anter
+ *
+ * Created on August 23, 2024, 1:03 PM
+ */
+
+#ifndef MCAL_INTERNAL_INTERRUPTS_H
+#define	MCAL_INTERNAL_INTERRUPTS_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MCAL_INTERNAL_INTERRUPTS_H */
+
