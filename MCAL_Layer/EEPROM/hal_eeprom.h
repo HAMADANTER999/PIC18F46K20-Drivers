f4ef154b3271bd60d27866196f67201d86b48d25
HAMADANTER999 <antermohammed390@gmail.com>
2024-08-27 21:31:44 +0300

    EEPROM Architechture completed

---
+++ MCAL_Layer/EEPROM/hal_eeprom.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   hal_eeprom.h
+ * Author: anter
+ *
+ * Created on August 27, 2024, 8:16 PM
+ */
+
+#ifndef HAL_EEPROM_H
+#define	HAL_EEPROM_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* HAL_EEPROM_H */
+
